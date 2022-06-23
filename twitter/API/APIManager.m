72082695a4745a3038247f5e9a3c5f7ca9e65136
Miguel Arriaga Velasco <marriagav@fb.com>
2022-06-23 11:55:51 -0700

    added profile picture to compose-tweet view

--- twitter/API/APIManager.m
+++ twitter/API/APIManager.m
@@ -8,6 +8,7 @@
 
 #import "APIManager.h"
 #import "Tweet.h"
+#import "User.h"
 
 static NSString * const baseURLString = @"https://api.twitter.com";
 
@@ -59,6 +60,19 @@ static NSString * const baseURLString = @"https://api.twitter.com";
            NSMutableArray *tweets = [Tweet tweetsWithArray:tweetDictionaries];
            completion(tweets, nil);
            self.arrayOfTweets = tweets;
+       } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+           // There was a problem
+           completion(nil, error);
+    }];
+}
+
+- (void)getUserInfo:(void (^)(User *user, NSError *error))completion{
+    [self GET:@"1.1/account/verify_credentials.json"
+       parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, NSDictionary *  _Nullable userDictionary) {
+           // Success
+        
+        User *user = [[User alloc] initWithDictionary:userDictionary];
+           completion(user, nil);
        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
            // There was a problem
            completion(nil, error);
--- twitter/View Controllers/ComposeViewController.m
+++ twitter/View Controllers/ComposeViewController.m
@@ -9,19 +9,38 @@
 #import "ComposeViewController.h"
 #import "APIManager.h"
 #import <QuartzCore/QuartzCore.h>
+#import "UIImageView+AFNetworking.h"
 
 @interface ComposeViewController () 
 @property (weak, nonatomic) IBOutlet UITextView *_tweetTextContent;
-
-
+@property (weak, nonatomic) IBOutlet UIImageView *_profilePicture;
 @end
 
 @implementation ComposeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self._tweetTextContent.layer.borderWidth = 1.0f;
+    self._tweetTextContent.layer.borderWidth = 0.5f;
     self._tweetTextContent.layer.borderColor = [[UIColor lightGrayColor] CGColor];
+    [self _setupProfilePicture];
+}
+
+- (void)_setupProfilePicture{
+    self._profilePicture.image = nil;
+    [[APIManager shared] getUserInfo: ^(User *user, NSError *error){
+        if (error) {
+            NSLog(@"Error getting home timeline: %@", error.localizedDescription);
+        } else {
+            if (user.profilePicture != nil) {
+                NSString *URLString = user.profilePicture;
+                NSURL *url = [NSURL URLWithString:URLString];
+                [self._profilePicture setImageWithURL:url];
+            }
+        }
+    }];
+    self._profilePicture.layer.cornerRadius = self._profilePicture.frame.size.height/2;
+    self._profilePicture.layer.borderWidth = 0;
+    self._profilePicture.clipsToBounds=YES;
 }
 
 - (void)_closeWindow{
